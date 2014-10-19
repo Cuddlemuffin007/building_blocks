b8c13a5f855a5ca2aaf5f075da9defe7bc547a17
Cuddlemuffin007 <brennonkrogers@gmail.com>
2014-10-19 14:55:31 +0900

    added my_all?, my_any?, my_none?, my_count methods

--- Enumerable_methods.rb
+++ Enumerable_methods.rb
@@ -15,11 +15,51 @@ module Enumerable
 	end
 
 	def my_select
+		return self unless block_given?
 		result = []
 		for i in self
 			yield(i) ? result.push(i) : i
 		end
-		return result
+		result
 	end
+
+	def my_all?
+		if block_given?
+			my_each{ |i| return false unless yield(i)}
+		else
+			my_each{ |i| return false unless i}
+		end
+		true
+	end
+
+	def my_any?
+		if block_given?
+			my_each{ |i| return true if yield(i)}
+		else
+			my_each{ |i| return true if i}
+		end
+		false
+	end
+
+	def my_none?
+		if block_given?
+			my_each{ |i| return false if yield(i)}
+		else
+			my_each{ |i| return false if i}
+		end
+		true
+	end
+
+	def my_count(arg = nil)
+		count = 0
+		if block_given?
+			my_each{ |i| count += 1 if yield(i)}
+		elsif arg.nil?
+			count = length
+		else
+			my_each{ |i| count += 1 if i == arg}
+		end
+		count
+	end			
 
 end
